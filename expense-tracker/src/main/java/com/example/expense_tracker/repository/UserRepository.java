eee50ef2876d258e6b5b352084e81a359dc5a53a
Omkar Patil <109729083+Omkar2703@users.noreply.github.com>
2026-08-08 15:53:30 +0530

    Add method to find user by username

--- expense-tracker/src/main/java/com/example/expense_tracker/repository/UserRepository.java
+++ expense-tracker/src/main/java/com/example/expense_tracker/repository/UserRepository.java
@@ -7,4 +7,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     // JpaRepository gives us save(), findAll(), findById(), deleteById() for free!
+    User findByUsername(String username);
 }
